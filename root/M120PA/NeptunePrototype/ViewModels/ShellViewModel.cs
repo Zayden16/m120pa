fbd1154059cb5f25b1ae7120cf7edd686c9c56cb
Yazdan Musa <yazdanmusa@gmail.com>
2020-05-29 11:44:48 +0200

    MPA7: Removed PHP Framework, Prototype with Caliburn Mirco

---
+++ root/M120PA/NeptunePrototype/ViewModels/ShellViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Caliburn.Micro;
+using NeptunePrototype.Models;
+
+namespace NeptunePrototype.ViewModels
+{
+	public class ShellViewModel : Screen
+	{
+		private string _firstName;
+		private string _lastName;
+		private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+		private PersonModel _selectedPerson;
+
+		public string FirstName
+		{
+			get { return _firstName; }
+			set
+			{
+				_firstName = value;
+				NotifyOfPropertyChange(() => FirstName);
+				NotifyOfPropertyChange(() => FullName);
+			}
+		}
+
+		public string LastName
+		{
+			get { return _lastName; }
+			set
+			{
+				_lastName = value;
+				NotifyOfPropertyChange(() => LastName);
+				NotifyOfPropertyChange(() => FullName);
+			}
+		}
+
+		public string FullName => $"{FirstName} {LastName}";
+
+		public BindableCollection<PersonModel> People
+		{
+			get { return _people; }
+			set { _people = value; }
+		}
+
+		public PersonModel SelectedPerson
+		{
+			get { return _selectedPerson; }
+			set
+			{
+				_selectedPerson = value;
+				NotifyOfPropertyChange(() => SelectedPerson);
+			}
+		}
+	}
+}
